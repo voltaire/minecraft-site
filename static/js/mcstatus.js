908b956538e7f0fe244cdc0fc231415b9092794b
Jon Chen <fly@sjchen.net>
2013-02-25 07:30:13 +0000

    relative path

--- static/js/mcstatus.js
+++ static/js/mcstatus.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
-    $('.mcstatus').load('http://voltaire.sh/mcstatus');
+    $('.mcstatus').load('/mcstatus');
     var refreshId = setInterval(function() {
-        $('.mcstatus').load('http://voltaire.sh/mcstatus');
+        $('.mcstatus').load('/mcstatus');
     }, 15000);
     $.ajaxSetup({ cache: false });
 });
