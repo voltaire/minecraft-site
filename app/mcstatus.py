74e7eee599dfcd05a9ea463b3a86bd3d98d1044f
Jon Chen <fly@sjchen.net>
2013-03-25 00:48:02 -0400

    add all the possible querys to mcstatus

--- app/mcstatus.py
+++ app/mcstatus.py
@@ -2,8 +2,20 @@ from minecraft_query import MinecraftQuery
 
 class mcstatus(MCSERVER_ADDR):
 
-    get_status = MinecraftQuery(MCSERVER_ADDR,25565).get_rules()
-    ONLINE_PLAYERS = get_status['players']
-    NUM_PLAYERS = get_status['numplayers']
-    MAX_PLAYERS = get_status['maxplayers']
+    query_mcserver = MinecraftQuery(MCSERVER_ADDR,25565).get_rules()
+
+    GAME_ID = query_mcserver['game_id']
+    GAMETYPE = query_mcserver['gametype']
+    MOTD = query_mcserver['motd']
+    MAP = query_mcserver['map']
+    MAX_PLAYERS = query_mcserver['maxplayers']
+    MCSERVER_HOSTNAME = query_mcserver['hostname']
+    MCSERVER_HOSTPORT = query_mcserver['hostport']
+    MCSERVER_PLAYERS = query_mcserver['players']
+    MCSERVER_PLUGINS = query_mcserver['plugins']
+    MCSERVER_RAWPLUGINS = query_mcserver['raw_plugins']
+    MCSERVER_SOFTWARE = query_mcserver['software']
+    MCSERVER_VERSION = query_mcserver['version']
+    NUM_PLAYERS = query_mcserver['numplayers']
+    ONLINE_PLAYERS = query_mcserver['players']
 
