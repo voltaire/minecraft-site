930b9e77be90b05286563fc0b0f5de8649484dff
Jon Chen <fly@sjchen.net>
2013-03-26 00:29:59 +0000

    get server address and port from url variables

--- app/views.py
+++ app/views.py
@@ -1,21 +1,30 @@
 from flask import render_template
 from app import app
-from mcstatus import mcstatus
 
 @app.route('/')
 def voltaire():
     NUMSERVERS = len(MCSERVERS)
 
-    return render_template('index.html', PAGE_TITLE='Dashboard', SITE_TITLE=SITE_TITLE, MCSERVERS=MCSERVERS, NUMSERVERS=NUMSERVERS)
+    return render_template('index.html',
+            PAGE_TITLE='Dashboard',
+            SITE_TITLE=SITE_TITLE, 
+            MCSERVERS=MCSERVERS, 
+            NUMSERVERS=NUMSERVERS)
 
 @app.route('/about')
 def about():
-    return render_template('about.html', SITE_TITLE=SITE_TITLE)
+    return render_template('about.html', 
+            SITE_TITLE=SITE_TITLE)
 
-@app.route('/mcstatus/<MCSTATUS>')
-def mcstatus(MCSTATUS):
-    global MCSERVER_ADDR
-    MCSERVER_ADDR = MCSTATUS
-
-    return render_template('mcstatus.html')
+@app.route('/mcstatus/<MCSERVER_ADDR>/<int:MCSERVER_PORT>')
+def return_mcstatus(MCSERVER_ADDR, MCSERVER_PORT):
+    get_status = MinecraftQuery(MCSERVER_ADDR,MCSERVER_PORT).get_rules()
+    ONLINE_PLAYERS = get_status['players']
+    NUM_PLAYERS = get_status['numplayers']
+    MAX_PLAYERS = get_status['maxplayers']
+    return render_template('mcstatus.html', 
+            MCSERVER_ADDR=MCSERVER_ADDR
+            ONLINE_PLAYERS=ONLINE_PLAYERS
+            NUM_PLAYERS = NUM_PLAYERS
+            MAX_PLAYERS = MAX_PLAYERS)
 
