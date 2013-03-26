89c1fa8ad80aebdc4d07cc213695955cd6b4f0e4
Jon Chen <fly@sjchen.net>
2013-03-26 00:28:59 +0000

    trying something better i hope

--- app/mcstatus.py
+++ app/mcstatus.py
@@ -2,20 +2,24 @@ from minecraft_query import MinecraftQuery
 
 class mcstatus:
 
-    global MCSERVER_ADDR
-    global MCSERVER_NAME
-    query_mcserver = MinecraftQuery(MCSERVER_ADDR,25565).get_rules()
+    def get_rules(MCSERVER_ADDR, QUERY_TYPE):
+        query_mcserver = MinecraftQuery(MCSERVER_ADDR,25565).get_rules()
 
-    GAME_ID = query_mcserver['game_id']
-    GAMETYPE = query_mcserver['gametype']
-    MOTD = query_mcserver['motd']
-    MAP = query_mcserver['map']
-    MAX_PLAYERS = query_mcserver['maxplayers']
-    MCSERVER_PLAYERS = query_mcserver['players']
-    MCSERVER_PLUGINS = query_mcserver['plugins']
-    MCSERVER_RAWPLUGINS = query_mcserver['raw_plugins']
-    MCSERVER_SOFTWARE = query_mcserver['software']
-    MCSERVER_VERSION = query_mcserver['version']
-    NUM_PLAYERS = query_mcserver['numplayers']
-    ONLINE_PLAYERS = query_mcserver['players']
+        """
+        GAME_ID = query_mcserver['game_id']
+        GAMETYPE = query_mcserver['gametype']
+        MOTD = query_mcserver['motd']
+        MAP = query_mcserver['map']
+        MAX_PLAYERS = query_mcserver['maxplayers']
+        MCSERVER_PLAYERS = query_mcserver['players']
+        MCSERVER_PLUGINS = query_mcserver['plugins']
+        MCSERVER_RAWPLUGINS = query_mcserver['raw_plugins']
+        MCSERVER_SOFTWARE = query_mcserver['software']
+        MCSERVER_VERSION = query_mcserver['version']
+        NUM_PLAYERS = query_mcserver['numplayers']
+        ONLINE_PLAYERS = query_mcserver['players']
+        """
+
+        return query_mcserver[?], [QUERY_TYPE]
+
 
