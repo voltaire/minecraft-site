4d71a39393483fb30891b6f9c68464dd0f5b8035
Jon Chen <fly@sjchen.net>
2013-03-26 17:38:16 +0000

    fix get status

--- app/views.py
+++ app/views.py
@@ -3,28 +3,41 @@ from app import app
 
 @app.route('/')
 def voltaire():
-    NUMSERVERS = len(MCSERVERS)
+    NUMSERVERS = len(app.config['MCSERVERS'])
+    """
+    d['name'] for d in app.config['MCSERVERS']:
+        try:
+            MCSERVER_NAME.append(d)
+
+        except NameError:
+            MCSERVER_NAME = [d]
+
+   dd['hostname'] for dd in app.config['MCSERVERS']:
+       try:
+           MCSERVER_ADDR.append(dd)
+
+       except NameError:
+           MCSERVER_NAME = [dd]
+    """
 
     return render_template('index.html',
             PAGE_TITLE='Dashboard',
-            SITE_TITLE=SITE_TITLE, 
-            MCSERVERS=MCSERVERS, 
+            SITE_TITLE='Voltaire',
             NUMSERVERS=NUMSERVERS)
 
 @app.route('/about')
 def about():
     return render_template('about.html', 
-            SITE_TITLE=SITE_TITLE)
+            SITE_TITLE='Voltaire')
 
 @app.route('/mcstatus/<MCSERVER_ADDR>/<int:MCSERVER_PORT>')
 def return_mcstatus(MCSERVER_ADDR, MCSERVER_PORT):
     get_status = MinecraftQuery(MCSERVER_ADDR,MCSERVER_PORT).get_rules()
+    """
     ONLINE_PLAYERS = get_status['players']
     NUM_PLAYERS = get_status['numplayers']
     MAX_PLAYERS = get_status['maxplayers']
+    """
     return render_template('mcstatus.html', 
-            MCSERVER_ADDR=MCSERVER_ADDR
-            ONLINE_PLAYERS=ONLINE_PLAYERS
-            NUM_PLAYERS = NUM_PLAYERS
-            MAX_PLAYERS = MAX_PLAYERS)
+            get_status = get_status)
 
