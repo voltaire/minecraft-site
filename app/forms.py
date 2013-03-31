c8598b5d196f480d294c5e0e4d27f5cf0630a225
Jon Chen <fly@sjchen.net>
2013-03-31 18:44:56 -0400

    regex to validate minecraft username

--- app/forms.py
+++ app/forms.py
@@ -2,12 +2,17 @@ from flask.ext.wtf import Form, TextField, BooleanField, Required, PasswordField
 
 class SignupForm(Form):
     mcuser = TextField('Minecraft Username', [
-        validators.Length(max=16),
-        validators.Required()
+        validators.Length(max=16, message='Username too long'),
+        validators.Regexp([a-zA-Z0-9_], message='Invalid characters in username' 
+        validators.Required(message='Enter your Minecraft Username')
         ])
     mcemail = TextField('Email Address', [
-        validators.Length(min=6, max=35),
-        validators.Required()
+        validators.Length(min=6, max=35, message='
+        Email needs to be between 6-35 characters'),
+        validators.Required(message='Enter your email address'),
+        validators.Email(message='Invalid email address')
         ])
-    accept_tos = BooleanField('I accept the TOS', [validators.Required()])
+    accept_tos = BooleanField('I accept the TOS', [
+        validators.Required(message='Accept the TOS')
+        ])
 
