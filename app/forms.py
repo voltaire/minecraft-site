a5747b5a96e7258392872be10c19c7b1b611088b
Jon Chen <fly@sjchen.net>
2013-03-31 19:13:56 -0400

    validate if minecraft username is premium or not

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,24 @@
 from flask.ext.wtf import Form, TextField, BooleanField, Required, PasswordField, validators
+import urllib
+
+# 'http://minecraft.net/haspaid.jsp?user=' + field.data
+def mcHasPaid(field):
+    message = '%s is not a Minecraft username or is not a paid Minecraft account.' % field.data
+
+    def _mcHasPaid(form, field):
+        mcun = field.data
+        param = urllib.quote(mcun)
+        f = urllib.urlopen("http://minecraft.net/haspaid.jsp?user=%s" % param)
+        if f.read() == 'false':
+            raise ValidationError(message)
+
+    return _mcHasPaid
 
 class SignupForm(Form):
     mcuser = TextField('Minecraft Username', [
         validators.Length(max=16, message='Username too long'),
-        validators.Regexp([a-zA-Z0-9_], message='Invalid characters in username' 
+        validators.Regexp([a-zA-Z0-9_], message='Invalid characters in username'),
+        mcHasPaid(),
         validators.Required(message='Enter your Minecraft Username')
         ])
     mcemail = TextField('Email Address', [
