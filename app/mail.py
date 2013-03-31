4cb0793d9b10b82239e493d0706e89ec27e618da
Jon Chen <fly@sjchen.net>
2013-03-31 17:13:39 -0400

    test mail

--- app/mail.py
+++ app/mail.py
@@ -1,2 +1,8 @@
 from flask.ext.mail import Mail, Message
 
+def testmail():
+    msg = Message("Hello", sender=("flask", "flask@voltaire.sh"), recipients=["sjchen@sjchen.net"])
+    msg.body = "testing"
+    msg.html = "<b>testing</b>"
+    mail.send(msg)
+
