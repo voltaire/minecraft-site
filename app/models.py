40e99ff34e540d4b9caba173bc3f4b63e8dffefc
Jon Chen <fly@sjchen.net>
2013-04-01 16:10:09 -0400

    ready to rock db

--- app/models.py
+++ app/models.py
@@ -13,3 +13,12 @@ class User(db.Model):
     def __repr__(self):
         return '<User %r>' % (self.mcName)
 
+class UserApplication(db.Model):
+
+    id = db.Column(db.Integer, primary_key = True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post %r>' % (self.body)
