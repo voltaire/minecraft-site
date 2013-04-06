01d0f510e9f6635f9c066d2e6f5a4a8b19c120a3
Jon Chen <fly@sjchen.net>
2013-04-06 20:14:33 +0000

    remove irrelevant commented bs

--- application/app/views.py
+++ application/app/views.py
@@ -8,21 +8,6 @@ from models import User, ROLE_USER, ROLE_MEMBER, ROLE_ADMIN
 @app.route('/')
 def voltaire():
     NUMSERVERS = len(app.config['MCSERVERS'])
-    """
-    d['name'] for d in app.config['MCSERVERS']:
-        try:
-            MCSERVER_NAME.append(d)
-
-        except NameError:
-            MCSERVER_NAME = [d]
-
-   dd['hostname'] for dd in app.config['MCSERVERS']:
-       try:
-           MCSERVER_ADDR.append(dd)
-
-       except NameError:
-           MCSERVER_NAME = [dd]
-    """
 
     return render_template('index.html',
             PAGE_TITLE='Dashboard',
@@ -37,11 +22,6 @@ def about():
 @app.route('/mcstatus/<MCSERVER_ADDR>/<int:MCSERVER_PORT>')
 def return_mcstatus(MCSERVER_ADDR, MCSERVER_PORT):
     get_status = MinecraftQuery(MCSERVER_ADDR,MCSERVER_PORT).get_rules()
-    """
-    ONLINE_PLAYERS = get_status['players']
-    NUM_PLAYERS = get_status['numplayers']
-    MAX_PLAYERS = get_status['maxplayers']
-    """
     return render_template('mcstatus.html', 
             get_status = get_status)
 
