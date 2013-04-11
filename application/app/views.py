ca7ae5106b852d8a7b1dec998f2143d29b18e8f1
Jon Chen <fly@sjchen.net>
2013-04-11 20:01:39 +0000

    init error pages

--- application/app/views.py
+++ application/app/views.py
@@ -64,3 +64,15 @@ def signup():
             title = 'Signup',
             form = form)
 
+@app.errorhandler(403)
+def page_forbidden(e):
+    return render_template('403.html'), 403
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500 
+
