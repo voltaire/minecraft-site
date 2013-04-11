4710a17745dc7929b2cd85d7453a50b510b1aa5e
Jon Chen <fly@sjchen.net>
2013-04-11 18:37:29 +0000

    add logging

--- application/app/__init__.py
+++ application/app/__init__.py
@@ -9,5 +9,37 @@ mail = Mail(app)
 db = SQLAlchemy(app)
 babel = Babel(app)
 
+if not app.debug:
+    import logging
+    from logging import Formatter, getLogger
+    from logging.handlers import RotatingFileHandler
+
+    file_handler = RotatingFileHandler('logs/errors.log')
+    file_handler.setLevel(logging.WARNING)
+
+    file_handler.setFormatter(Formatter(
+        '%(asctime)s %(levelname)s: %(message)s '
+        '[in %(pathname)s:%(lineno)d]'))
+
+    mail_handler.setFormatter(Formatter('''
+        Message type:       %(levelname)s
+        Location:           %(pathname)s:%(lineno)d
+        Module:             %(module)s
+        Function:           %(funcName)s
+        Time:               %(asctime)s
+
+        Message:
+
+        %(message)s
+        '''))
+
+    app.log.addHandler(file_handler)
+    app.log.addHandler(mail_handler)
+
+    loggers = [app.logger, getLogger('sqlalchemy')]
+    for logger in loggers:
+        logger.addHandler(mail_handler)
+        logger.addHandler(file_handler)
+
 from app import views, models
 
