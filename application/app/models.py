e1d2d59d1b13ec2165555ba06201a0a0a7d528a4
Jon Chen <fly@sjchen.net>
2013-04-29 18:52:11 -0400

    use mcemail instead of email

--- application/app/models.py
+++ application/app/models.py
@@ -6,18 +6,18 @@ ROLE_ADMIN = 1
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    mcName = db.Column(db.String(16), index = True, unique = True)
-    email = db.Column(db.String(120), index = True, unique = True)
+    mcuser = db.Column(db.String(16), index = True, unique = True)
+    mcemail = db.Column(db.String(120), index = True, unique = True)
     applicant_ip = db.Column(db.String(120), index = True, unique = True)
     #role = db.Column(db.SmallInteger, default = ROLE_USER)
 
-    def __init__(self, mcName=None, email=None, applicant_ip=None):
-      self.mcName = mcName
-      self.email = email
+    def __init__(self, mcuser=None, mcemail=None, applicant_ip=None):
+      self.mcuser = mcuser
+      self.mcemail = mcemail
       self.applicant_ip = applicant_ip
 
     def __repr__(self):
-        return '<User %r>' % (self.mcName)
+        return '<User %r>' % (self.mcuser)
 
 class UserApplication(db.Model):
 
