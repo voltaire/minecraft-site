b742ca3291430412e5980d557c4b3b6ef4ef6bbb
Jon Chen <fly@sjchen.net>
2014-05-04 18:55:21 -0400

    remove unused babel extension

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,11 @@
 from flask import Flask
 from flask.ext.mail import Mail
 from flask.ext.sqlalchemy import SQLAlchemy
-from flask.ext.babel import Babel
 
 app = Flask(__name__)
 app.config.from_object('config')
 mail = Mail(app)
 db = SQLAlchemy(app)
-babel = Babel(app)
 
 if not app.debug:
     import logging
