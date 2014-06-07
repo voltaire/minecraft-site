c41fc241512d78f08899a5d30992585241e78483
Jon Chen <fly@sjchen.net>
2014-06-07 18:43:15 -0400

    remove unused js

--- static/js/mcstatus.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    $('.mcstatus').load('/mcstatus');
-    var refreshId = setInterval(function() {
-        $('.mcstatus').load('/mcstatus');
-    }, 15000);
-    $.ajaxSetup({ cache: false });
-});
